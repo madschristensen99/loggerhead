2bb99570fe0fc6576e2b9a73cab37070161c5dad
Thomas Ieong <th.ieong@free.fr>
2025-07-04 22:46:20 +0200

    backend: add comment explaining reasoning of this route

--- backend/routes/currency.js
+++ backend/routes/currency.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET currency listing. */
+// Ideally we want this to return the probability of if it's better to invest in eur
+// or usd
 router.get('/', function(req, res, next) {
   res.json({ EUR: '70%', USD: '30%' });
 });
