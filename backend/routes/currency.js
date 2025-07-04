1c9d7c4ce54585956904c714376d983f4a37dcba
Thomas Ieong <th.ieong@free.fr>
2025-07-04 22:50:25 +0200

    backend: Test sample Flipside request per the docs

--- backend/routes/currency.js
+++ backend/routes/currency.js
@@ -4,6 +4,25 @@ var router = express.Router();
 /* GET currency listing. */
 // Ideally we want this to return the probability of if it's better to invest in eur
 // or usd
+
+const { Flipside } = require("@flipsidecrypto/sdk");
+
+const flipside = new Flipside(
+  process.env.FLIPSIDE_API_KEY,
+  "https://api-v2.flipsidecrypto.xyz"
+);
+
+const sql = `
+SELECT
+  date_trunc('hour', block_timestamp) as hour,
+  count(distinct tx_hash) as tx_count
+FROM ethereum.core.fact_transactions
+WHERE block_timestamp >= GETDATE() - interval'7 days'
+GROUP BY 1
+`
+
+const queryResultSet = await flipside.query.run({sql: sql});
+
 router.get('/', function(req, res, next) {
   res.json({ EUR: '70%', USD: '30%' });
 });
