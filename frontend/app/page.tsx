bdffd9b49025df001e798fc5aab680865387dce6
imbjdd <b0bb1e@protonmail.com>
2025-07-04 22:46:07 +0200

    feat: add login

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,44 +1,50 @@
 "use client";
 
-import { useState } from "react";
+import { usePrivy } from '@privy-io/react-auth';
 
 export default function Home() {
-  const [wallet, setWallet] = useState<string>("");
+  const { ready, authenticated, login, logout, user } = usePrivy();
 
-  const handleWallet = () => {
-    if(wallet) {
-      setWallet('');
-    } else {
-      setWallet('uwu');
-    }
-  };
+  if (!ready) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-xl">Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div className="w-screen min-h-screen h-full">
       <nav className="fixed top-0 left-0 right-0 bg-white">
         <div className="max-w-7xl mx-auto p-4 flex justify-between items-center">
           <p className="font-bold text-xl">LoggerHead</p>
-          <button className="font-bold text-xl px-4 py-2 bg-black text-white rounded-md" onClick={handleWallet}>Wallet</button>
+          <button 
+            className="font-bold text-xl px-4 py-2 bg-black text-white rounded-md" 
+            onClick={authenticated ? logout : login}
+          >
+            {authenticated ? 'Disconnect' : 'Connect Wallet'}
+          </button>
         </div>
       </nav>
-      {!wallet && (
-      <div className="flex items-center justify-center h-[calc(100vh)]">
-      <div className="max-w-2xl w-full px-4">
-        <div className="flex flex-col gap-2">
-          <p className="text-xl">Please connect to your account</p>
-        </div>
-      </div>
-    </div>
-      )}
-      {wallet && (
-      <div className="flex items-center justify-center h-[calc(100vh)]">
-        <div className="max-w-2xl w-full px-4">
-          <div className="flex flex-col gap-2">
-            <p className="text-xl">Your Balance</p>
-            <h1 className="text-7xl font-bold">1,234<span className="text-black/30">.23</span>$</h1>
+      {!authenticated && (
+        <div className="flex items-center justify-center h-[calc(100vh)]">
+          <div className="max-w-2xl w-full px-4">
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Please connect to your account</p>
+            </div>
           </div>
         </div>
-      </div>
+      )}
+      {authenticated && (
+        <div className="flex items-center justify-center h-[calc(100vh)]">
+          <div className="max-w-2xl w-full px-4">
+            <div className="flex flex-col gap-2">
+              <p className="text-xl">Your Balance</p>
+              <p>{user?.id}</p>
+              <h1 className="text-7xl font-bold">1,234<span className="text-black/30">.23</span>$</h1>
+            </div>
+          </div>
+        </div>
       )}
     </div>
   );
---
+++ frontend/app/provider.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import {PrivyProvider} from '@privy-io/react-auth';
+
+export default function Providers({children}: {children: React.ReactNode}) {
+  return (
+    <PrivyProvider
+      appId="cmcp6mwhf01lll40mdz8dl046"
+      config={{
+        // Create embedded wallets for users who don't have a wallet
+        embeddedWallets: {
+          ethereum: {
+            createOnLogin: 'users-without-wallets'
+          }
+        }
+      }}
+    >
+      {children}
+    </PrivyProvider>
+  );
+}
