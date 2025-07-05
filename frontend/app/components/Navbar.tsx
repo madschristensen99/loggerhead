9cd1c0618d8802d7f50de4a58b7e2b8b1fb2f780
imbjdd <b0bb1e@protonmail.com>
2025-07-06 01:47:57 +0200

    feat: add navbar

--- frontend/app/components/Navbar.tsx
+++ frontend/app/components/Navbar.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 import { Square3Stack3DIcon, ChartBarIcon, NewspaperIcon } from '@heroicons/react/24/outline';
+import { usePrivy } from '@privy-io/react-auth';
 
 export default function Navbar() {
+  const { authenticated, logout, login } = usePrivy();
+
   return (
     <header className="w-full">
       <div className="max-w-7xl mx-auto px-0 py-6 flex justify-between items-center">
-        <h1 className="text-2xl font-black tracking-wider">LOGGERHEAD</h1>
+        <Link href="/">
+          <h1 className="text-2xl font-black tracking-wider">LOGGERHEAD</h1>
+        </Link>
         <div className="flex items-center">
           <div className="flex gap-6 items-center bg-pink-200 rounded-full px-6 py-2 mr-6">
             <Link href="/balance" className="flex items-center gap-2">
@@ -21,9 +26,15 @@ export default function Navbar() {
               News
             </Link>
           </div>
-          <Link href="/app" className="flex items-center gap-2 bg-black text-white px-8 py-2 rounded-full hover:bg-gray-800 transition-colors">
-            Start Now
-          </Link>
+          {authenticated ? (
+            <button onClick={logout} className="flex items-center gap-2 bg-black text-white px-8 py-2 rounded-full hover:bg-gray-800 transition-colors">
+              Logout
+            </button>
+          ) : (
+            <button onClick={login} className="flex items-center gap-2 bg-black text-white px-8 py-2 rounded-full hover:bg-gray-800 transition-colors">
+              Login
+            </button>
+          )}
         </div>
       </div>
     </header>
